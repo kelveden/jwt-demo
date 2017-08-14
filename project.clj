e9039c010a6da0d6dfec4cd44702cdb0073e60a8
Alistair Dutton <alistair.dutton@ovoenergy.com>
2017-08-14 16:56:04 +0100

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject jwt-demo "0.0.1-SNAPSHOT"
+  :description "JWT Demo"
+  :dependencies [[pandect "0.6.1"]
+                 [cheshire "5.7.1"]
+                 [org.clojure/clojure "1.8.0"]]
+  :source-paths ["src/clj"]
+  :profiles {:dev {:source-paths ["dev/clj"]
+                   :repl-options {:init-ns jwt-demo}}})
